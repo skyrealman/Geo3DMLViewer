6547bbe5c3f56c11a087e570092f8129053b5ce8
song zhen <skyrealman@imac.lan>
2016-09-01 11:27:07 +0800

    first

---
+++ Geo3DMLViewer/Geo3DMLViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Geo3DMLViewer
+//
+//  Created by skyrealman on 16/9/1.
+//  Copyright © 2016年 skyrealman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
